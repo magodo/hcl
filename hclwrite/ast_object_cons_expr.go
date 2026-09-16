0a4f19fdbc3ae8a9d69b2ead1352613f0331d146
magodo <wztwcy@gmail.com>
2026-09-16 21:32:38 +1000

    Fix SetItem bug

--- hclwrite/ast_object_cons_expr.go
+++ hclwrite/ast_object_cons_expr.go
@@ -88,7 +88,7 @@ func (object *ObjectConsExpr) SetItem(key string, expr *Expression) (*ObjectCons
 	} else {
 		item = newObjectConsItem()
 		item.init(key, expr)
-		object.items.Add(object.children.Append(item))
+		object.items.Add(object.children.Insert(object.children.last, item))
 	}
 	return item.kv()
 }
